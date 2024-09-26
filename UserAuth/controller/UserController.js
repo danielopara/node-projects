b9bdfc7626a75b488ba4db58000a621a490bd98b
Daniel Opara <oparadaniv@gmail.com>
2024-09-26 17:02:00 +0100

    adding get by.getUserByFirstName);

--- UserAuth/controller/UserController.js
+++ UserAuth/controller/UserController.js
@@ -94,6 +94,7 @@ const getUserById = async (req, res, next) => {
   }
 };
 
+//update user by id
 const updateUserById = async (req, res) => {
   const { firstName, lastName, email } = req.body;
   const { id } = req.params;
@@ -132,9 +133,43 @@ const updateUserById = async (req, res) => {
   }
 };
 
+const getUserByFirstName = async (req, res) => {
+  const { firstName } = req.params;
+
+  try {
+    const user = await User.findOne({
+      where: { firstName: firstName.toLowerCase() },
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        status: 404,
+        message: "user not found",
+      });
+    }
+
+    return res.status(200).json({
+      status: 200,
+      message: "user retrieved",
+      data: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      status: 500,
+      message: "Server error",
+    });
+  }
+};
+
 module.exports = {
   createUser,
   getUser,
   getUserById,
   updateUserById,
+  getUserByFirstName,
 };
--- UserAuth/route/UserRoute.js
+++ UserAuth/route/UserRoute.js
@@ -3,8 +3,11 @@ const userController = require("../controller/UserController");
 const router = express.Router();
 
 router.post("/create", userController.createUser);
+
 router.get("/users", userController.getUser);
+router.get("/user/:firstName", userController.getUserByFirstName);
 router.get("/user/:id", userController.getUserById);
+
 router.put("/update-user/:id", userController.updateUserById);
 
 module.exports = router;
