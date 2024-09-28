07b87b93ea8c26670c5a937fd4b7b77e9198bd23
Daniel Opara <oparadaniv@gmail.com>
2024-09-28 12:00:51 +0100

    fixed hashing problem and adding login functionality

--- UserAuthWithSecurity/controllers/UserController.js
+++ UserAuthWithSecurity/controllers/UserController.js
@@ -1,19 +1,13 @@
 import { User } from "../models/UserModel.js";
 import bcrypt from "bcrypt";
 import { logFailure, logSuccess } from "../utils/Logging.js";
-import {
-  decryptPasswordAES,
-  encryptPasswordAES,
-} from "../config/SecurityConfig.js";
 
 export const createUser = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
   try {
-    const encryptedPassword = encryptPasswordAES(password);
-
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(encryptedPassword, salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const user = await User.create({
       firstName,
@@ -33,7 +27,48 @@ export const createUser = async (req, res) => {
       },
     });
   } catch (err) {
-    logSuccess(err);
+    logFailure(err);
+    return res.status(500).json({
+      status: 500,
+      message: "ERROR",
+    });
+  }
+};
+
+export const loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({
+        status: 404,
+        message: "user not found",
+      });
+    }
+
+    const hashedPassword = user.password;
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+
+    if (!isMatch) {
+      return res.status(404).json({
+        status: 404,
+        message: "invalid password",
+      });
+    }
+
+    return res.status(200).json({
+      status: 200,
+      message: "success",
+      user: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    logFailure(err);
     return res.status(500).json({
       status: 500,
       message: "ERROR",
--- UserAuthWithSecurity/routes/UserRoute.js
+++ UserAuthWithSecurity/routes/UserRoute.js
@@ -1,8 +1,9 @@
-import { createUser } from "../controllers/UserController.js";
+import { createUser, loginUser } from "../controllers/UserController.js";
 import express from "express";
 
 const userRoutes = express.Router();
 
 userRoutes.post("/createUser", createUser);
+userRoutes.post("/login", loginUser);
 
 export default userRoutes;
