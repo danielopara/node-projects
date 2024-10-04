546c78fef7e6aca9a9017a86f4aedbea405f00d6
Daniel Opara <oparadaniv@gmail.com>
2024-10-04 23:30:14 +0100

    created the user model

---
+++ fileUpload/src/models/UserModel.js
@@ -0,0 +1,35 @@
+import { sequelize } from "../config/DbConfig.js";
+import { DataTypes } from "sequelize";
+
+const User = sequelize.define(
+  "User",
+  {
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "email must not be empty",
+        },
+      },
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "password must not be empty",
+        },
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: (user) => {
+        user.email = user.email.toLowercase();
+      },
+    },
+  }
+);
